2009949f54c8a6914403a78a804acda4d3f6bb6e
Kristian Ullrich <ullrich@evolbio.mpg.de>
2022-06-03 09:32:58 +0200

    Update random_ratio_tss.py

--- gtf_gff3/random_ratio_tss.py
+++ gtf_gff3/random_ratio_tss.py
@@ -143,35 +143,53 @@ def create_bed(k, gtf_type, pos, o_dir, bam_header_file, seqlen=100):
     return(inside_sorted, outside_sorted)
 
 
-def get_ratio_TSS(inside_bed, outside_bed, o_dir, input_bam, k, gtf_type, bam_header_file, verbose = True): 
+def get_ratio_TSS(inside_bed, outside_bed, o_dir, input_bams, k, gtf_type, bam_header_file, verbose = True): 
 ## the idea would be to first calculate the average coverage per sample for in and out beds. Calculate each ratio
 ## get the maximum the ratios across replicates and return it as a dictionary
-    if verbose:
-        print("parse gtf_type %s on chromosome %s on BAM file %s" % (gtf_type, k, input_bam))
     out_TSS_tmp = o_dir + "/" + k + "_" + gtf_type + ".ratio_TSS.tmp"    
     out_TSS_file = o_dir + "/" + k + "_" + gtf_type + ".ratio_TSS.tsv"
     in_bed = pybedtools.BedTool(inside_bed)
     out_bed = pybedtools.BedTool(outside_bed)
-    in_cov = in_bed.coverage(input_bam, sorted=True, g=bam_header_file)
-    out_cov = out_bed.coverage(input_bam, sorted=True, g=bam_header_file)
-    inside_df = pandas.DataFrame(columns=['id','inside'])
-    for entry in in_cov:
-        new_entry = pandas.DataFrame({'id' : [entry.name] , 'inside' : [float(entry[6])]})
-        inside_df = pandas.concat([inside_df,new_entry], ignore_index=True)
-    outside_df = pandas.DataFrame(columns=['id','outside'])
-    for entry in out_cov:
-        new_entry = pandas.DataFrame({'id' : [entry.name] , 'outside' : [float(entry[6])]})
-        outside_df = pandas.concat([outside_df, new_entry], ignore_index=True)
-    merged = pandas.merge(inside_df, outside_df, on="id")
-    merged['ratio_TSS'] = (merged['inside']+0.01)/(merged['outside']+0.01)
-    merged['ratio_TSS'] = pandas.to_numeric(merged['ratio_TSS'])
+    merged_dict = {}
+    merged_all = pandas.DataFrame(columns=['id'])
+    for input_bam in input_bams:
+        if verbose:
+            print("parse gtf_type %s on chromosome %s on BAM file %s" % (gtf_type, k, input_bam))
+        in_cov = in_bed.coverage(input_bam, sorted=True, g=bam_header_file)
+        out_cov = out_bed.coverage(input_bam, sorted=True, g=bam_header_file)
+        inside_df = pandas.DataFrame(columns=['id','inside'])
+        for entry in in_cov:
+            new_entry = pandas.DataFrame({'id' : [entry.name] , 'inside' : [float(entry[6])]})
+            inside_df = pandas.concat([inside_df, new_entry], ignore_index=True)
+        outside_df = pandas.DataFrame(columns=['id','outside'])
+        for entry in out_cov:
+            new_entry = pandas.DataFrame({'id' : [entry.name] , 'outside' : [float(entry[6])]})
+            outside_df = pandas.concat([outside_df, new_entry], ignore_index=True)
+        merged = pandas.merge(inside_df, outside_df, on="id")
+        merged['ratio_TSS'] = (merged['inside']+0.01)/(merged['outside']+0.01)
+        merged['ratio_TSS'] = pandas.to_numeric(merged['ratio_TSS'])
+        merged_dict[input_bam] = merged
+    merged_all['id'] = merged_dict[input_bams[0]]['id']
+    for input_bam in input_bams:
+        merged_all[input_bam] = merged_dict[input_bam]['ratio_TSS']
+    merged_all.set_index('id', inplace=True)
+    merged_all['max'] = merged_all.max(axis=1)
     #os.system('rm {i} {o}'.format(i=inside_bed, o=outside_bed))
-    merged.to_csv(out_TSS_tmp, sep = "\t", header=False)
+    merged_all.to_csv(out_TSS_tmp, sep = "\t", header=False)
     os.system("paste {i} {o} {t} > {m}".format(i=inside_bed, o=outside_bed, t=out_TSS_tmp, m=out_TSS_file))
     os.system('rm {i} {o} {t}'.format(i=inside_bed, o=outside_bed, t=out_TSS_tmp))
+    title = ['INSIDE_CHROM', 'INSIDE_START', 'INSIDE_END', 'INSIDE_ID', 'INSIDE_SCORE', 'INSIDE_STRAND',
+    'OUTSIDE_CHROM', 'OUTSIDE_START', 'OUTSIDE_END', 'OUTSIDE_ID', 'OUTSIDE_SCORE', 'OUTSIDE_STRAND',
+    'ID']
+    title = title + input_bams + ['MAX_RATIO']
+    title = '\t'.join(title) + '\n'
+    with open(out_TSS_file, 'r+') as file:
+        content = file.read()
+        file.seek(0)
+        file.write(title + content)
 
 
-def get_random_tss(input_bam, output_dir, reference, reference_gtf, min_size, random_tss_per_chr, mask, gtf_feature, not_parse_reference, write_reference_Ns, verbose):
+def get_random_tss(input_bams, output_dir, reference, reference_gtf, min_size, random_tss_per_chr, mask, gtf_feature, not_parse_reference, write_reference_Ns, verbose):
     # create output directory
     if not os.path.exists(output_dir):
         os.mkdir(output_dir)
@@ -210,8 +228,8 @@ def get_random_tss(input_bam, output_dir, reference, reference_gtf, min_size, ra
     gtf_introns = gtf_introns.subtract(mask)
     gtf_subset_merge = gtf_subset_merge.subtract(mask)
     if verbose:
-        print("read BAM header %s" % input_bam)
-    bam_header_file = get_bam_header2(input_bam, output_dir)
+        print("read BAM header %s" % input_bams[0])
+    bam_header_file = get_bam_header2(input_bams[0], output_dir)
     bam_header = read_bam_header(bam_header_file, min_size)
     # for each chromosome create bed file
     for k in list(bam_header.keys()):
@@ -224,28 +242,28 @@ def get_random_tss(input_bam, output_dir, reference, reference_gtf, min_size, ra
         if len(gtf_tss_k) > 0:
             gtf_tss_k_pos = get_sample_position(gtf_tss_k, random_tss_per_chr)
             tss_inside_bed, tss_outside_bed = create_bed(k, "random_tss", gtf_tss_k_pos, output_dir, bam_header_file)
-            get_ratio_TSS(tss_inside_bed, tss_outside_bed, output_dir, input_bam, k, "random_tss", bam_header_file, verbose)
+            get_ratio_TSS(tss_inside_bed, tss_outside_bed, output_dir, input_bams, k, "random_tss", bam_header_file, verbose)
         if len(gtf_tes_k) > 0:
             gtf_tes_k_pos = get_sample_position(gtf_tes_k, random_tss_per_chr)
             tes_inside_bed, tes_outside_bed = create_bed(k, "random_tes", gtf_tes_k_pos, output_dir, bam_header_file)
-            get_ratio_TSS(tes_inside_bed, tes_outside_bed, output_dir, input_bam, k, "random_tes", bam_header_file, verbose)
+            get_ratio_TSS(tes_inside_bed, tes_outside_bed, output_dir, input_bams, k, "random_tes", bam_header_file, verbose)
         if len(gtf_introns_k) > 0:
             gtf_introns_k_pos = get_sample_position(gtf_introns_k, random_tss_per_chr)
             introns_inside_bed, introns_outside_bed = create_bed(k, "random_introns", gtf_introns_k_pos, output_dir, bam_header_file)
-            get_ratio_TSS(introns_inside_bed, introns_outside_bed, output_dir, input_bam, k, "random_introns", bam_header_file, verbose)
+            get_ratio_TSS(introns_inside_bed, introns_outside_bed, output_dir, input_bams, k, "random_introns", bam_header_file, verbose)
         if len(gtf_subset_merge_k) > 0:
             gtf_subset_merge_k_pos = get_sample_position(gtf_subset_merge_k, random_tss_per_chr)
             feature_inside_bed, feature_outside_bed = create_bed(k, "random_feature", gtf_subset_merge_k_pos, output_dir, bam_header_file)
-            get_ratio_TSS(feature_inside_bed, feature_outside_bed, output_dir, input_bam, k, "random_feature", bam_header_file, verbose)
+            get_ratio_TSS(feature_inside_bed, feature_outside_bed, output_dir, input_bams, k, "random_feature", bam_header_file, verbose)
 
 
 if __name__ == "__main__":
     from argparse import ArgumentParser
     parser = ArgumentParser("random ratio_TSS")
-    parser.add_argument("input_bam", help="Input BAM")
     parser.add_argument("output_dir", help="Output directory")
     parser.add_argument("reference", help="Reference FASTA")
     parser.add_argument("reference_gtf", help="Reference GTF")
+    parser.add_argument("--input_bams", help="Input BAM files", nargs='+')
     parser.add_argument("--min_size", type=int, default=100000, help="Minimum chr/scaffold/contig size in bp (default: 100000)")
     parser.add_argument("--random_tss_per_chr", type=int, default=1000, help="Number of random TSS per chr/scaffold/contig (default: 1000)")
     parser.add_argument("--mask", help="Specify sites to be masked in BED format (CHR, START, END, STRAND)")
@@ -255,5 +273,5 @@ if __name__ == "__main__":
     parser.add_argument("--verbose", help="increase output verbosity (default: False)", action="store_true")
     args = parser.parse_args()
     print(args)
-    get_random_tss(args.input_bam, args.output_dir, args.reference, args.reference_gtf, args.min_size, args.random_tss_per_chr, args.mask, args.gtf_feature, args.not_parse_reference, args.write_reference_Ns, args.verbose)
+    get_random_tss(args.input_bams, args.output_dir, args.reference, args.reference_gtf, args.min_size, args.random_tss_per_chr, args.mask, args.gtf_feature, args.not_parse_reference, args.write_reference_Ns, args.verbose)
 
