7bb832deb28f2066910b5ef0aabecf0dd4ad3eb0
Kristian Ullrich <ullrich@evolbio.mpg.de>
2025-04-18 04:36:17 +0200

    Update orthodb2sample.py

--- utils/orthodb2sample.py
+++ utils/orthodb2sample.py
@@ -46,10 +46,12 @@ def define_parser():
     parser.add_argument('-species', help='input file: odb12v1_species.tab.gz')
     parser.add_argument('-og2genes', help='input file: odb12v1_OG2genes.tab.gz')
     parser.add_argument('-og_pairs', help='input file: odb12v1_OG_pairs.tab.gz')
+    parser.add_argument('-genes', help='input file: odb12v1_genes.tab.gz')
     parser.add_argument('-sl', help='output file: species_list.tsv', default='species_list.tsv')
     parser.add_argument('-oc', help='output file: Orthogroups.GeneCount.tsv', default='Orthogroups.GeneCount.tsv')
     parser.add_argument('-og', help='output file: Orthogroups.tsv', default='Orthogroups.tsv')
     parser.add_argument('-use_filtered_ogs', action='store_true', help='use only the filtered set of orthogroups (most ancestral + isolated)')
+    parser.add_argument('-use_original_protein_id', action='store_true', help='not use Ortho DB unique gene id but instead protein original sequence id')
     return parser
 
 
@@ -86,15 +88,6 @@ def get_filtered_sample_ogs(args, parser, sample_ogs):
     completely_isolated = sample_ogs - involved_ogs
     filtered_sample_ogs = (sample_ogs & most_ancestral).union(completely_isolated)
     return filtered_sample_ogs
-
-
-def create_species_list(args, parser, samples):
-    with open(args.sl, 'w') as fo:
-        with gzip.open(args.species, 'rt') as f:
-            for line in f:
-                tax_id, species_id, *_ = line.strip().split('\t')
-                if species_id in samples:
-                    fo.write(species_id + '\t' + tax_id + '\n')
 
 
 def get_sample_oc_og(args, parser, sample_to_index, sample_ogs):
@@ -145,15 +138,46 @@ def filter_empty_columns(data_dict, samples, is_counts=True):
     return filtered_data, filtered_samples
 
 
-def write_tsv(data_dict, samples, output_path, is_counts=True):
+def create_species_list(args, parser, samples):
+    with open(args.sl, 'w') as fo:
+        with gzip.open(args.species, 'rt') as f:
+            for line in f:
+                tax_id, species_id, *_ = line.strip().split('\t')
+                if species_id in samples:
+                    fo.write(species_id + '\t' + tax_id + '\n')
+
+
+def get_gene_to_protein_mapping(args, parser):
+    mapping = {}
+    with gzip.open(args.genes, 'rt') as f:
+        for line in f:
+            parts = line.strip().split('\t')
+            if len(parts) < 3:
+                continue
+            gene_id, species_id, protein_id, *_ = parts
+            if species_id == args.species_id:
+                mapping[gene_id] = protein_id
+    return mapping
+
+
+def write_tsv(data_dict, samples, output_path, is_counts=True, gene_to_protein=None, target_species_idx=None):
     with open(output_path, 'w') as f:
         header = ['Orthogroup'] + samples + ['Total'] if is_counts else ['Orthogroup'] + samples
         f.write('\t'.join(header) + '\n')
         for og, values in sorted(data_dict.items()):
-            total = str(sum(values)) if is_counts else None
-            row = [og] + [str(val) for val in values]
-            if total:
-                row.append(total)
+            if is_counts:
+                total = str(sum(values))
+                row = [og] + [str(val) for val in values] + [total]
+            else:
+                new_values = []
+                for idx, val in enumerate(values):
+                    if gene_to_protein and idx == target_species_idx and val:
+                        ids = val.split(',')
+                        mapped = [gene_to_protein.get(gid, gid) for gid in ids]
+                        new_values.append(','.join(mapped))
+                    else:
+                        new_values.append(val)
+                row = [og] + new_values
             f.write('\t'.join(row) + '\n')
 
 
@@ -175,6 +199,12 @@ def main():
     if args.og_pairs is None:
         parser.print_help()
         sys.exit('Please specify odb12v1_OG_pairs.tab.gz file path')
+    if args.genes is None:
+        parser.print_help()
+        sys.exit('Please specify odb12v1_genes.tab.gz file path')
+    gene_to_protein = None
+    if args.use_original_protein_id:
+        gene_to_protein = get_gene_to_protein_mapping(args, parser)
     samples = get_samples(args, parser)
     sample_to_index = {name: idx for idx, name in enumerate(samples)}
     sample_ogs = get_sample_ogs(args, parser)
@@ -185,14 +215,15 @@ def main():
     sample_og_filtered, samples_filtered_genes = filter_empty_columns(sample_og, samples, is_counts=False)
     assert samples_filtered_counts == samples_filtered_genes, "Mismatch in filtered sample columns"
     #Write species list
-    samples_to_use = samples_filtered_counts if args.use_filtered_ogs else samples
-    create_species_list(args, parser, samples_to_use)
+    create_species_list(args, parser, samples_filtered_counts)
     # Write zipped gene counts
+    sample_to_index_filtered = {name: idx for idx, name in enumerate(samples_filtered_genes)}
+    target_species_idx = sample_to_index_filtered.get(args.species_id)
     #write_tsv(sample_oc, samples, args.oc, is_counts=True)
     write_tsv(sample_oc_filtered, samples_filtered_counts, args.oc, is_counts=True)
     # Write zipped gene identifiers
     #write_tsv(sample_og, samples, args.og, is_counts=False)
-    write_tsv(sample_og_filtered, samples_filtered_genes, args.og, is_counts=False)
+    write_tsv(sample_og_filtered, samples_filtered_genes, args.og, is_counts=False, gene_to_protein=gene_to_protein, target_species_idx=target_species_idx)
     print(f"Files written: {args.oc}, {args.og}")
 
 
