7bb0e6610f9d6b4381d96995aed2e49faa6fd4b4
Kristian Ullrich <ullrich@evolbio.mpg.de>
2025-05-06 15:05:18 +0200

    Update polarizeVCFbyOutgroup.py

--- vcf/polarizeVCFbyOutgroup.py
+++ vcf/polarizeVCFbyOutgroup.py
@@ -8,7 +8,7 @@ date: May 2025
 email: ullrich@evolbio.mpg.de
 License: MIT
 The MIT License (MIT)
-Copyright (c) 2021 Kristian Ullrich
+Copyright (c) 2025 Kristian Ullrich
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
 in the Software without restriction, including without limitation the rights
